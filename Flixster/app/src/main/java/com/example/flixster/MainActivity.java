d845a92f43fd1313fbaa5288ae70a3d2fbc48cb3
Ana Martinez <61203063+AnaMart1nez@users.noreply.github.com>
2020-09-26 00:35:14 -0400

    Flixster Part 1
    Networking and Parsing JSON
    RecyclerView implemented
    land/port mode implemented
    UI color improvements

---
+++ Flixster/app/src/main/java/com/example/flixster/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.adapters.MovieAdapter;
+import com.example.flixster.models.Movie;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Create a get request on the currently playing movies
+    // Notice the api_key is embedded on this URL
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    //Add a tag to easily log data Jump to the "Log" statement to see...
+    public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
+
+
+        // Create an adapter
+        final MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+
+        //Set an adapter in the recycler view
+        rvMovies.setAdapter(movieAdapter);
+
+        // set a layout manager in the recycler view
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        //Create an instance of our async http client
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        //We want to make sure we are using the JSON response handler because the current API is using JSON
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.i(TAG, "Movies: " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hint: json exception", e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
+    }
+}
